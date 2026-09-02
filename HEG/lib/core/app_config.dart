8cbabc6036eded6351dae9de26ce2b6fd0d8f57c
anchit <anchitchourasia78@gmail.com>
2026-09-02 08:45:54 +0530

    2

--- HEG/lib/core/app_config.dart
+++ HEG/lib/core/app_config.dart
@@ -1,25 +1,9 @@
-// class AppConfig {
-//   static const String apiBaseUrl = 'http://127.0.0.1:9092/vpms';
-//   static const String cvpsBaseUrl = 'http://127.0.0.1:9092/cvps';
-
-//   static const String dummyGateLogBaseUrl = 'http://localhost:8080/cvps';
-//   static const String dummyGateLogApiKey = 'HEG_12345_SECRET';
-
-//   static const String apiKey = 'VPMS_SECRET_KEY_2026';
-// }
 class AppConfig {
-  static const String apiBaseUrl = 'http://192.168.9.130:9092/vpms';
-  static const String cvpsBaseUrl = 'http://192.168.9.130:9092/cvps';
+  static const String apiBaseUrl = 'http://127.0.0.1:9092/vpms';
+  static const String cvpsBaseUrl = 'http://127.0.0.1:9092/cvps';
 
-  static const String dummyGateLogBaseUrl = 'http://10.0.2.2:8080/cvps';
+  static const String dummyGateLogBaseUrl = 'http://192.168.8.25:8080/cvps';
   static const String dummyGateLogApiKey = 'HEG_12345_SECRET';
 
   static const String apiKey = 'VPMS_SECRET_KEY_2026';
 }
-
-// class AppConfig {
-//   static const String apiBaseUrl = 'http://192.168.9.130:9092/vpms';
-//   static const String cvpsBaseUrl = 'http://192.168.9.130:9092/cvps';
-//   static const String dummyGateLogBaseUrl = 'http://10.0.2.2:3033/cvps';
-//   static const String apiKey = 'VPMS_SECRET_KEY_2026';
-// }
