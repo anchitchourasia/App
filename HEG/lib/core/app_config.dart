0cefa3297fc022b4dfd4825c55354595c1a6ef9b
anchit <anchitchourasia78@gmail.com>
2026-09-05 15:59:41 +0530

    2

--- HEG/lib/core/app_config.dart
+++ HEG/lib/core/app_config.dart
@@ -4,7 +4,7 @@ class AppConfig {
   // static const String cvpsBaseUrl = 'http://127.0.0.1:9092/cvps';
   static const String cvpsBaseUrl = 'http://192.168.9.130:9092/cvps';
 
-  static const String dummyGateLogBaseUrl = 'http://192.168.8.25:8080/cvps';
+  static const String dummyGateLogBaseUrl = 'http://localhost:8080/cvps';
   static const String dummyGateLogApiKey = 'HEG_12345_SECRET';
 
   static const String apiKey = 'VPMS_SECRET_KEY_2026';
